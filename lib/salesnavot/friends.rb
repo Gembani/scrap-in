bd8f6bca410bbec48726e74464e3b8d121d9cad6
Nicholas Stock <nick@gembani.com>
2018-04-12 15:11:54 +0200

    fix friends list

--- lib/salesnavot/friends.rb
+++ lib/salesnavot/friends.rb
@@ -11,7 +11,7 @@ module Salesnavot
       @session.driver.browser.execute_script(script, element.native)
     end
     def css(count)
-      "ul.mn-connections__list > div >  li:nth-child(#{count+3})"
+      "section.mn-connections > ul >  li:nth-child(#{count+1})"
     end
 
     def get_next_block(count)
@@ -20,7 +20,7 @@ module Salesnavot
         puts "block == nil: #{count}"
         scroll_to(@session.all(:css, css(count - 1)).first)
         while (@session.all(:css, css(count)).first == nil)
-          puts "sleeping"
+          puts "#{css(count)} not found"
           sleep(0.1)
         end
         block = @session.all(:css, css(count)).first
@@ -34,7 +34,7 @@ module Salesnavot
       puts "execute find friends"
       num_times.times do
         block = get_next_block(count)
-        name = block.find("a.mn-person-info__link").find(".mn-person-info__name").text
+        name = block.find(".mn-connection-card__name").text
         time_ago = block.find(".time-ago").native.text
         yield time_ago, name
         count = count + 1
@@ -44,9 +44,8 @@ module Salesnavot
     def init_list
       puts "init list"
       @session.visit("https://www.linkedin.com/mynetwork/invite-connect/connections/")
-      
-      while (@session.all('.mn-person-card').count == 0)
-        puts "sleeping"
+      while (@session.all('.mn-connection-card').count == 0)
+        puts ".mn-connection-card not found"
         sleep(0.1)
       end
 
--- spec/salesnavot_spec.rb
+++ spec/salesnavot_spec.rb
@@ -27,12 +27,12 @@ RSpec.describe Salesnavot do
     session.driver.quit
   end
 
-  xit "creates lead" do
+  it "creates lead" do
     lead = session.new_lead({sales_nav_url: "https://www.linkedin.com/sales/profile/568261266,esdT,NAME_SEARCH?"})
     lead.scrap
   end
 
-  it 'create invite already connected' do
+  xit 'create invite already connected' do
     invite = session.invite("https://www.linkedin.com/sales/profile/323951533,F1Ig,NAME_SEARCH?moduleKey=peopleSearchResults&pageKey=sales-search3-people&contextId=8F37C172A38F1315806C569E8B2B0000&requestId=f9372319-4f38-4bae-9830-e810398675f5&action=CLICK&target=urn%3Ali%3AsalesLead%3A(-1%2C323951533)&pageNumber=0&targetEl=profilelink&position=7&trk=lss-serp-result-lead_name")
     if invite.execute
       puts "invite sent"
@@ -47,16 +47,16 @@ RSpec.describe Salesnavot do
     end
   end
 
-  it 'from linkedin profile send message' do
+  xit 'from linkedin profile send message' do
     session.send_message('https://www.linkedin.com/in/scebula/',
       'Hi, this is a test message at ' + Time.now.strftime("%H:%M:%S").to_s + ". Thanks!")
   end
 
-  # it 'scrap friends' do
-  #   session.sent_invites.execute(200) do |time_str, name|
-  #     puts time_str, name
-  #   end
-  # end
+  it 'scrap friends' do
+    session.friends.execute(200) do | name|
+      puts  name
+    end
+  end
 
   it 'scrap profile views' do
     session.sent_invites.execute(200) do |time_str, name|
