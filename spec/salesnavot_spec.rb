d588daadbac864b6ef849c5bab89410c0748cae4
Seybol <sebastien.cebula@gmail.com>
2018-05-16 15:28:19 +0200

    Added a test: Scrap sent invites on multiples pages

--- spec/salesnavot_spec.rb
+++ spec/salesnavot_spec.rb
@@ -37,9 +37,17 @@ RSpec.describe Salesnavot do
     expect(lead.linkedin_url).not_to be_nil
   end
 
-  it 'sent_invites ' do
+  it 'sent_invites up to 40 (less than one page)' do
     session.sent_invites.execute(40) do |invite|
       puts invite
+    end
+  end
+
+  it 'sent_invites up to 230 (more than one page)' do
+    count = 1
+    session.sent_invites.execute(230) do |invite|
+      puts count.to_s + " -> " + invite.to_s
+      count = count + 1
     end
   end
 
@@ -48,6 +56,7 @@ RSpec.describe Salesnavot do
       puts invite
     end
   end
+
   xit 'create invite already connected' do
     invite = session.invite("https://www.linkedin.com/sales/profile/323951533,F1Ig,NAME_SEARCH?moduleKey=peopleSearchResults&pageKey=sales-search3-people&contextId=8F37C172A38F1315806C569E8B2B0000&requestId=f9372319-4f38-4bae-9830-e810398675f5&action=CLICK&target=urn%3Ali%3AsalesLead%3A(-1%2C323951533)&pageNumber=0&targetEl=profilelink&position=7&trk=lss-serp-result-lead_name")
     if invite.execute
