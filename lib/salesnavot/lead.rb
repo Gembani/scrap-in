eff69db517b9e3794cc2819f1e9264efc21dada1
Sébastien CEBULA <sebastien.cebula@gmail.com>
2018-07-21 17:47:04 +0200

    Completed Lead Class

--- lib/salesnavot/lead.rb
+++ lib/salesnavot/lead.rb
@@ -1,57 +1,82 @@
 module Salesnavot
+  # Goes to lead profile and scrap his phones, emails and websites
   class Lead
     attr_reader :name, :emails,
-      :phone_number, :url, :linkedin_url,
-      :sales_nav_url, :links, :phones
+                :phone_number, :url, :linkedin_url,
+                :sales_nav_url, :links, :phones
     def initialize(config, session)
-      @sales_nav_url = config[:sales_nav_url] || ""
+      @sales_nav_url = config[:sales_nav_url] || ''
       @emails = config[:emails] || []
       @phones = config[:phones] || []
       @first_degree = config[:first_degree] || false
       @links = config[:links] || []
-      @linkedin_url = config[:linkedin_url] || ""
-      @name = config[:name] || ""
       @session = session
     end
 
-    def to_hash
-      {name: @name, linkedin_url: @linkedin_url, phones: @phones, first_degree: @first_degree, emails: @emails, links: @links, sales_nav_url: @sales_nav_url}
+    def phone_selector
+      '.contact-info-form__phone div.contact-info-form__phone-readonly-group'
     end
 
-    def to_json
-      to_hash.to_json
+    def email_selector
+      '.contact-info-form__email div.contact-info-form__email-readonly-group'
+    end
+
+    def link_selector
+      '.contact-info-form__link div.contact-info-form__website-readonly-group'
+    end
+
+    def infos_selector
+      'button.profile-topcard__contact-info-show-all'
+    end
+
+    def scrap_phones
+      return unless @session.has_selector?(phone_selector, wait: 1)
+      phones = @session.all(phone_selector)
+      phones.each do |phone|
+        value = phone.find('.contact-info-form__phone-readonly-text a').text
+        @phones << value
+      end
+    end
+
+    def scrap_emails
+      return unless @session.has_selector?(email_selector, wait: 1)
+      emails = @session.all(email_selector)
+      emails.each do |email|
+        value = email.find('.contact-info-form__email-readonly-text a').text
+        @emails << value
+      end
+    end
+
+    def scrap_links
+      return unless @session.has_selector?(link_selector, wait: 1)
+      links = @session.all(link_selector)
+      links.each do |link|
+        value = link.find('.contact-info-form__website-readonly-text a').text
+        @links << value
+      end
+    end
+
+    def scrap_linkedin_url
+      @session.find('button.profile-topcard-actions__overflow-toggle').click
+      @session.find('a.view-linkedin').click
+      @linkedin_url = IO.popen('pbpaste', 'r+').read
+    end
+
+    def scrap_datas
+      @session.find('button.profile-topcard__contact-info-show-all').click
+      %w[phones emails links].each do |name|
+        method("scrap_#{name}").call
+      end
     end
 
     def scrap
       @session.visit @sales_nav_url
-      sleep(4)
-      @name = @session.find(".profile-topcard-person-entity__name").text
-      byebug
-      @first_degree = ('1st' ==  @session.find('.m-type--degree').text)
-      if @session.all(".contact-info-button").count == 0
-        puts "No contact info button"
-        return
-      end
-      @session.all(".contact-info-button").first.click
-      sleep(1)
+      @name = @session.find('.profile-topcard-person-entity__name').text
+      @first_degree = (@session.find('.m-type--degree').text == '1st')
+      return unless @session.has_selector?(infos_selector)
 
-      @session.all("div.more-info-tray table").each do |table|
-        if (table.all("th").count == 1 && table.find("th").text == "Phone")
-            table.all("td ul li").each do |item|
-              @phones << item.text
-            end
-        else
-          table.all("a").each do |link|
-            if link.native.attribute(:href).include?("mailto:")
-              @emails << link.native.attribute(:href).gsub("mailto:", "")
-            elsif link.native.attribute(:href) == link.native.text
-              @linkedin_url = link.native.text
-            else
-              @links << {name: link.native.text, url: link.native.attribute(:href)}
-            end
-          end
-        end # if (table.find("th").text == "Phone")
-      end
+      scrap_linkedin_url
+      scrap_datas
     end
   end
 end
