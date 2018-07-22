f554a8949a373163290b20958bd70e82fc48c1c4
Sébastien CEBULA <sebastien.cebula@gmail.com>
2018-07-22 21:41:19 +0200

    Modified ProfileView Class. All has changed...

--- lib/salesnavot/profile_views.rb
+++ lib/salesnavot/profile_views.rb
@@ -1,58 +1,64 @@
 module Salesnavot
+  # Goes to 'profile views' page and get all persons who viewed our profile
   class ProfileViews
+    attr_reader :profile_viewed_by
     def initialize(session)
       @session = session
+      @profile_viewed_by = []
     end
+
     def scroll_to(element)
       script = <<-JS
         arguments[0].scrollIntoView(true);
       JS
-
       @session.driver.browser.execute_script(script, element.native)
     end
+
     def css(count)
-      "section.me-wvmp-viewers-list article:nth-child(#{count+1})"
+      'section.me-wvmp-viewers-list '\
+      "article[data-control-name=\"profileview_single\"]:nth-child(#{count})"
     end
 
-    def get_next_block(count)
-      block = @session.all(:css, css(count)).first
-      if (block == nil)
-        puts "block == nil: #{count}"
-        scroll_to(@session.all(:css, css(count - 1)).first)
-        while (@session.all(:css, css(count)).first == nil)
-          puts "sleeping"
-          sleep(0.2)
-        end
-        block = @session.all(:css, css(count)).first
-      end
-      block
+    def last_element_css(count)
+      'section.me-wvmp-viewers-list '\
+       "article[data-control-name=\"profileview_private\"]:nth-child(#{count})"
     end
 
-    def execute(num_times = 40)
-      init_list
-      count = 0
-      num_times.times do
-        block = get_next_block(count)
-        if  block.all(".actor-name-with-distance span:nth-child(1)").count == 0
-          puts "No name found for #{block.text}"
+    def find_name_and_time_ago(number)
+      item = @session.find(css(number))
+      scroll_to(item)
+      name = item.find('.me-wvmp-viewer-card__name-text').text
+      time_ago = item.find('.me-wvmp-viewer-card__time-ago').text
+      yield name, time_ago
+      @profile_viewed_by.push name
+    end
+
+    def execute(num_times = 50)
+      return unless init_list(target_page)
+
+      count = 1
+      i = 1
+      not_a_lead = 0
+      while count <= num_times
+        i = not_a_lead + count
+        if @session.has_selector?(css(i), wait: 1)
+          find_name_and_time_ago(i) { |name, time_ago| yield name, time_ago }
+          count += 1
         else
-          name = block.find(".actor-name-with-distance span:nth-child(1)").text
-          time_ago = block.find(".me-wvmp-viewer-card__time-ago").text
-          yield time_ago, name
+          break if @session.all(last_element_css(i)).count == 1
+          not_a_lead += 1
         end
-        count = count + 1
-
       end
     end
 
-    def init_list
-      @session.visit("https://www.linkedin.com/me/profile-views/")
-      while (@session.all(:css, css(0)).first == nil)
-        sleep(0.1)
-        puts 'Searching for css...'
-      end
-
+    def target_page
+      'https://www.linkedin.com/me/profile-views/'
     end
 
+    def init_list(link)
+      @session.visit(link)
+      return false unless @session.has_selector?('section.me-wvmp-viewers-list')
+      true
+    end
   end
 end
