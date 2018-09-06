3ae9f2ae3fed54fa6232dd10244bf434535aa7c5
Sébastien CEBULA <sebastien.cebula@gmail.com>
2018-09-06 13:00:27 +0200

    Email requirement is now detected. Added new errors messages

--- lib/salesnavot/invite.rb
+++ lib/salesnavot/invite.rb
@@ -1,71 +1,88 @@
 module Salesnavot
-class Invite
-  attr_reader :error
-  def initialize(sales_nav_url,session, content)
+  class Invite
+    attr_reader :error
+    def initialize(sales_nav_url, session, content)
       @sales_nav_url = sales_nav_url
       @session = session
-      @error = ""
+      @error = ''
       @content = content
-  end
+    end
 
-  def go_to(link)
-    @session.visit(link)
-    return false unless @session.has_selector?('.profile-topcard')
-    true
-  end
+    def go_to(link)
+      @session.visit(link)
+      return false unless @session.has_selector?('.profile-topcard')
+      true
+    end
 
-  def is_friend?
-    if @session.has_selector?('.m-type--degree', wait: 4)
-      if '1st' ==  @session.find('.m-type--degree').text
-        return true
-      else
-        @error = "Already friends"
+    def friend?
+      if @session.has_selector?('.m-type--degree', wait: 4)
+        if @session.find('.m-type--degree').text == '1st'
+          return true
+        else
+          @error = 'Already friends'
+          return false
+        end
+      end
+      @error = 'No connections. Not a friend'
+      false
+    end
+
+    def find_and_click(css)
+      unless @session.has_selector?(css)
+        @error = "Cannot find action button for css = #{css}"
         return false
       end
+      @session.find(css).click
+      true
     end
-    @error = "No connections. Not a friend"
-    return false
-  end
 
-  def find_and_click(css)
-    unless @session.has_selector?(css)
-      @error = "Cannot find action button for css = #{css}"
-      return false
+    def action_button_css
+      '.profile-topcard-actions__overflow-toggle'
     end
-    @session.find(css).click
-    return true
-  end
 
-  def action_button_css
-    '.profile-topcard-actions__overflow-toggle'
-  end
+    def connect_button_css
+      '.connect'
+    end
 
-  def connect_button_css
-    '.connect'
-  end
+    def send_button_css
+      '.button-primary-medium'
+    end
 
-  def send_button_css
-    '.button-primary-medium'
-  end
+    def lead_email_required?
+      if @session.has_selector?('#connect-cta-form__email')
+        @error = "Lead's email is required to connect"
+        return false
+      end
+      true
+    end
 
-  def click_and_connect
-    return false unless find_and_click(action_button_css)
-    return false unless find_and_click(connect_button_css)
-    @session.fill_in 'connect-cta-form__invitation', with: @content
-    find_and_click(send_button_css)
-  end
+    def invitation_window_closed?
+      !@session.has_selector?('connect-cta-form')
+    end
 
-  def lead_invited?
-    return false unless find_and_click(action_button_css)
-    @session.has_selector?('.pending-connection')
-  end
+    def click_and_connect
+      return false unless find_and_click(action_button_css)
+      return false unless find_and_click(connect_button_css)
+      return false unless lead_email_required?
+      @session.fill_in 'connect-cta-form__invitation', with: @content
+      find_and_click(send_button_css)
+    end
 
-  def execute
-    return false unless go_to(@sales_nav_url)
-    return false if is_friend?
-    return false unless click_and_connect
-    lead_invited?
-  end
-end
+    def lead_invited?
+      return false unless invitation_window_closed?
+      return false unless find_and_click(action_button_css)
+      unless @session.has_selector?('.pending-connection')
+        @error = "Can't find .pending connection button"
+        return false
+      end
+      true
+    end
 
+    def execute
+      return false unless go_to(@sales_nav_url)
+      return false if friend?
+      return false unless click_and_connect
+      lead_invited?
+    end
+  end
 end
