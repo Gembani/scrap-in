ec8dd13bf59222125c46646ef4a96b8549c62b57
Sébastien CEBULA <sebastien.cebula@gmail.com>
2018-09-06 17:08:58 +0200

    Revert "added debugs ..."
    This reverts commit ba05e8fe80fe3dd1bd47256cb1dceab915a6d3a5.

--- lib/salesnavot/friends.rb
+++ lib/salesnavot/friends.rb
@@ -34,27 +34,20 @@ module Salesnavot
       init_list
       count = 0
       num_times.times do
-        puts 'Loop'
         friend = get_next_friend(count)
-        puts 'before condition'
         if friend && @session.has_selector?(friend_name_css) &&
             @session.has_selector?(time_ago_css)
-          puts 'condition ok'
           name = friend.find(friend_name_css).text
           time_ago = friend.find(time_ago_css).text
           yield time_ago, name
         end
         count += 1
       end
-      puts 'End execute'
     end
 
     def init_list
-      puts 'Before Visit'
       @session.visit('https://www.linkedin.com/mynetwork/invite-connect/connections/')
-      puts 'After Visit'
       unless @session.has_selector?(nth_friend_css(0))
-        puts 'Error a'
         @error = "No friend found (no css element: #{nth_friend_css(0)})"
         return false
       end
