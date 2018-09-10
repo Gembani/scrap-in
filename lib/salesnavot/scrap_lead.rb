b85972a557e89733980d599339fcc9bc6c8c16ee
Sébastien CEBULA <sebastien.cebula@gmail.com>
2018-09-10 16:03:37 +0200

    Refactored ScrapLead class

--- lib/salesnavot/scrap_lead.rb
+++ lib/salesnavot/scrap_lead.rb
@@ -2,8 +2,11 @@ module Salesnavot
   # Goes to lead profile and scrap his phones, emails and websites
   class ScrapLead
     include Tools
+    include CssSelectors::ScrapLead
+
     attr_reader :name, :emails, :url, :linkedin_url,
                 :sales_nav_url, :links, :phones, :error
+
     def initialize(config, session)
       @sales_nav_url = config[:sales_nav_url] || ''
       @emails = config[:emails] || []
@@ -12,6 +15,22 @@ module Salesnavot
       @links = config[:links] || []
       @session = session
       @error = ''
+    end
+
+    def execute
+      @session.visit @sales_nav_url
+
+      return false unless find_lead_name
+      find_lead_degree
+      return false unless scrap_datas
+      byebug
+    end
+
+    def scrap_datas
+      return false unless find_and_click(infos_css)
+      %w[phones emails links].each do |name|
+        method("scrap_#{name}").call
+      end
     end
 
     def to_hash
@@ -29,79 +48,48 @@ module Salesnavot
       to_hash.to_json
     end
 
-    def phone_selector
-      '.contact-info-form__phone div.contact-info-form__phone-readonly-group'
-    end
-
-    def email_selector
-      '.contact-info-form__email div.contact-info-form__email-readonly-group'
-    end
-
-    def link_selector
-      '.contact-info-form__link div.contact-info-form__website-readonly-group'
-    end
-
-    def infos_selector
-      'button.profile-topcard__contact-info-show-all'
-    end
-
     def scrap_phones
-      return unless @session.has_selector?(phone_selector, wait: 1)
-      phones = @session.all(phone_selector)
+      return unless @session.has_selector?(phones_block_css, wait: 1)
+      phones = @session.all(phones_block_css)
       phones.each do |phone|
-        value = phone.find('.contact-info-form__phone-readonly-text a').text
+        value = phone.find(phone_css).text
         @phones << value
       end
     end
 
     def scrap_emails
-      return unless @session.has_selector?(email_selector, wait: 1)
-      emails = @session.all(email_selector)
+      return unless @session.has_selector?(emails_block_css, wait: 1)
+      emails = @session.all(emails_block_css)
       emails.each do |email|
-        value = email.find('.contact-info-form__email-readonly-text a').text
+        value = email.find(email_css).text
         @emails << value
       end
     end
 
     def scrap_links
-      return unless @session.has_selector?(link_selector, wait: 1)
-      links = @session.all(link_selector)
+      return unless @session.has_selector?(links_block_css, wait: 1)
+      links = @session.all(links_block_css)
       links.each do |link|
-        value = link.find('.contact-info-form__website-readonly-text a').text
+        value = link.find(link_css).text
         @links << value
       end
     end
 
     def find_lead_name
-      unless @session.has_selector?('.profile-topcard-person-entity__name')
+      unless @session.has_selector?(name_css)
         @error = 'No name found'
         return false
       end
-      @name = @session.find('.profile-topcard-person-entity__name').text
+      @name = @session.find(name_css).text
       true
     end
 
     def find_lead_degree
-      unless @session.has_selector?('.m-type--degree')
+      unless @session.has_selector?(degree_css)
         @first_degree = false
         return
       end
-      @first_degree = (@session.find('.m-type--degree').text == '1st')
-    end
-
-    def scrap_datas
-      return false unless find_and_click(infos_selector)
-      %w[phones emails links].each do |name|
-        method("scrap_#{name}").call
-      end
-    end
-
-    def execute
-      @session.visit @sales_nav_url
-
-      return false unless find_lead_name
-      find_lead_degree
-      return false unless scrap_datas
+      @first_degree = (@session.find(degree_css).text == '1st')
     end
   end
 end
