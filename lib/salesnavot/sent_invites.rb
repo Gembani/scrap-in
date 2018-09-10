984959c94881a5b162631c3b5a9cf5f01116200d
Sébastien CEBULA <sebastien.cebula@gmail.com>
2018-09-10 17:08:21 +0200

    Refactored SentInvite class

--- lib/salesnavot/sent_invites.rb
+++ lib/salesnavot/sent_invites.rb
@@ -2,6 +2,7 @@ module Salesnavot
   # Goes to "Sent invitations" page, and scrap all leads that were invited
   class SentInvites
     include Tools
+    include CssSelectors::SentInvites
     attr_reader :invited_leads
     def initialize(session)
       @session = session
@@ -12,24 +13,14 @@ module Salesnavot
       'https://www.linkedin.com/mynetwork/invitation-manager/sent/'
     end
 
-    def nth_invited_lead_css(count, invitation: true)
-      if invitation
-        ".mn-invitation-list li:nth-child(#{count + 1}) .invitation-card__name"
-      else
-        ".mn-invitation-list li:nth-child(#{count + 1})"
-      end
-    end
-
     def find_lead_name(count)
-      if @session.has_selector?(nth_invited_lead_css(count), wait: 3)
-        item = @session.find(nth_invited_lead_css(count))
-        scroll_to(item)
-        name = item.text
-        unless name.empty?
-          @invited_leads.push name
-          yield name
-        end
-      end
+      return unless @session.has_selector?(nth_lead_css(count), wait: 3)
+      item = @session.find(nth_lead_css(count))
+      scroll_to(item)
+      name = item.text
+      return if name.empty?
+      @invited_leads.push name
+      yield name
     end
 
     def execute(num_times = 50)
@@ -37,7 +28,9 @@ module Salesnavot
 
       count = 0
       num_times.times.each do
-        unless @session.has_selector?(nth_invited_lead_css(count, invitation: false), wait: 10)
+        unless @session.has_selector?(
+          nth_lead_css(count, invitation: false), wait: 10
+        )
           count = 0
           break unless next_page
         end
@@ -48,21 +41,14 @@ module Salesnavot
 
     def init_list(link)
       @session.visit(link)
-      return false unless @session.has_selector?('.mn-invitation-list')
+      return false unless @session.has_selector?(invitation_list_css)
       true
-    end
-
-    def pagination_selector
-      'a.mn-invitation-pagination__control-btn'
     end
 
     def find_next_button
       pagination_buttons = @session.all(pagination_selector)
       pagination_buttons.each do |button|
-        next unless button.has_selector?(
-          'li-icon[aria-label="Next item"]',
-          wait: 1
-        )
+        next unless button.has_selector?(next_button_css, wait: 1)
         return button
       end
       nil
