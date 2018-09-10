4bd476048c4c2e748b91ba1c4004580a30316d14
Sébastien CEBULA <sebastien.cebula@gmail.com>
2018-09-10 13:04:44 +0200

    Refactored and completed Friend Class

--- lib/salesnavot/friends.rb
+++ lib/salesnavot/friends.rb
@@ -1,12 +1,41 @@
 module Salesnavot
+  # Go to friend list, and get their names
+  # and when you connected with them
   class Friends
     include Tools
+    include CssSelectors::Friends
     def initialize(session)
       @session = session
     end
 
-    def nth_friend_css(count)
-      "section.mn-connections > ul >  li:nth-child(#{count + 1})"
+    def search_for_name_and_time_ago(count)
+      friend = get_next_friend(count)
+      if friend && @session.has_selector?(friend_name_css) &&
+         @session.has_selector?(time_ago_css)
+        name = friend.find(friend_name_css).text
+        time_ago = friend.find(time_ago_css).text
+        yield time_ago, name
+      end
+    end
+
+    def execute(num_times = 40)
+      init_list
+      count = 0
+      num_times.times do
+        search_for_name_and_time_ago(count) do |name, time_ago|
+          yield name, time_ago
+        end
+        count += 1
+      end
+    end
+
+    def init_list
+      @session.visit(connections_url)
+      unless @session.has_selector?(nth_friend_css(0))
+        @error = "No friend found (no css element: #{nth_friend_css(0)})"
+        return false
+      end
+      true
     end
 
     def get_next_friend(count)
@@ -14,38 +43,6 @@ module Salesnavot
       friend = @session.find(nth_friend_css(count))
       scroll_to(friend)
       friend
-    end
-
-    def friend_name_css
-      '.mn-connection-card__name'
-    end
-
-    def time_ago_css
-      '.time-ago'
-    end
-
-    def execute(num_times = 40)
-      init_list
-      count = 0
-      num_times.times do
-        friend = get_next_friend(count)
-        if friend && @session.has_selector?(friend_name_css) &&
-           @session.has_selector?(time_ago_css)
-          name = friend.find(friend_name_css).text
-          time_ago = friend.find(time_ago_css).text
-          yield time_ago, name
-        end
-        count += 1
-      end
-    end
-
-    def init_list
-      @session.visit('https://www.linkedin.com/mynetwork/invite-connect/connections/')
-      unless @session.has_selector?(nth_friend_css(0))
-        @error = "No friend found (no css element: #{nth_friend_css(0)})"
-        return false
-      end
-      true
     end
   end
 end
