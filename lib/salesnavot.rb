06f15f9f639c2083c592502bb5970489859a9560
Sébastien CEBULA <sebastien.cebula@gmail.com>
2018-09-10 12:10:05 +0200

    Modified strings from "" to '' -> No interpolation here

--- lib/salesnavot.rb
+++ lib/salesnavot.rb
@@ -1,24 +1,23 @@
-require "salesnavot/version"
+require 'salesnavot/version'
 
-require "salesnavot/helpers/tools"
+require 'salesnavot/helpers/tools'
 
-require "capybara/dsl"
-require "salesnavot/search"
-require "salesnavot/scrap_lead"
-require "salesnavot/invite"
-require "salesnavot/sent_invites"
-require "salesnavot/friends"
-require "salesnavot/profile_views"
-require "salesnavot/session"
-require "salesnavot/auth"
-require "salesnavot/driver"
-require "salesnavot/send_message"
-require "salesnavot/threads"
-require "salesnavot/messages"
+require 'capybara/dsl'
+require 'salesnavot/search'
+require 'salesnavot/scrap_lead'
+require 'salesnavot/invite'
+require 'salesnavot/sent_invites'
+require 'salesnavot/friends'
+require 'salesnavot/profile_views'
+require 'salesnavot/session'
+require 'salesnavot/auth'
+require 'salesnavot/driver'
+require 'salesnavot/send_message'
+require 'salesnavot/threads'
+require 'salesnavot/messages'
 
 module Salesnavot
   def self.setup
     Capybara.run_server = false
-
   end
 end
