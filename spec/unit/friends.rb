81da346d920fb40fdd94ba680d31cb2b989adbf6
Sébastien CEBULA <sebastien.cebula@gmail.com>
2018-09-11 17:18:02 +0200

    Added Unit tests for Friends class

---
+++ spec/unit/friends.rb
@@ -0,0 +1,58 @@
+require 'spec_helper'
+
+RSpec.describe Salesnavot::Friends do
+
+  let(:friends) do
+    described_class.new(session)
+  end
+
+  let(:session) { instance_double('Capybara::Session') }
+
+  let(:nth_friend_css) { 'nth_friend_css' }
+  describe 'Initializer' do
+    subject { described_class }
+    it { is_expected.to eq Salesnavot::Friends }
+  end
+
+  describe 'instance of described class' do
+    subject { friends.instance_variables }
+    it { is_expected.to include(:@session) }
+  end
+
+  describe '.execute' do
+    context 'Friends were found on the page' do
+      before do
+        allow(friends).to receive(:visit_target_page).and_return(true)
+        allow(friends).to receive(:search_for_name_and_time_ago).and_yield(
+          Faker::Name.name,
+          Faker::Time.backward(14, :evening)
+        )
+      end
+
+      it 'finds one friend' do
+        friends.execute(num_times = 1) do |name, time_ago|
+          expect(name).not_to be_empty
+          expect(time_ago.to_s).not_to be_empty
+        end
+      end
+      it 'finds multiple friends' do
+        friends.execute do |name, time_ago|
+          expect(name).not_to be_empty
+          expect(time_ago.to_s).not_to be_empty
+        end
+      end
+    end
+    context 'Friends were not found on the page' do
+      before do
+        allow(friends).to receive(:nth_friend_css).with(0).and_return(nth_friend_css)
+        allow(session).to receive(:visit).with(friends.connections_url)
+        allow(session).to receive(:has_selector?).with(nth_friend_css)
+      end
+
+      it 'stops the research and return false' do
+        friends.execute
+        expect(friends.error).not_to be_empty
+      end
+    end
+  end
+end
