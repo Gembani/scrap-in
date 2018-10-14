8f25e9adab99ca2c050d5aa8d3c5bd34cb8416e0
Nicholas Stock <nick@gembani.com>
2018-10-14 19:19:16 +0300

    lets us react to different error messages differently.

--- lib/salesnavot/invite.rb
+++ lib/salesnavot/invite.rb
@@ -7,23 +7,34 @@ module Salesnavot
     def initialize(sales_nav_url, session, content)
       @sales_nav_url = sales_nav_url
       @session = session
-      @error = ''
+      @error_types = [:already_pending, :out_of_network, :already_friends, :email_required, :invitattion_form_didnot_close]
+      @error_messages = {
+        already_pending: 'Invitation is already pending ...',
+        out_of_network: 'Lead is out of network.',
+        already_friends: 'Already friends',
+        email_required:  "Lead's email is required to connect",
+        invitattion_form_didnot_close: 'Invitation form did not close'
+      }
+      @error = nil
       @content = content
     end
-
+    def error_message
+      return nil if @error.nil?
+      @error_messages[@error]
+    end
     def execute
       if @sales_nav_url.include?('OUT_OF_NETWORK')
-        @error = 'Lead is out of network.'
+        @error = :out_of_network
         return false
       end
       visit_target_page(@sales_nav_url)
       if initially_pending?
-        @error = 'Invitation is already pending ...'
+        @error = :already_pending
         return false
       end
       find_and_click(action_button_css)
       if friend?
-        @error = 'Already friends'
+        @error = :already_friends
         return false
       end
       return false unless click_and_connect
@@ -51,7 +62,7 @@ module Salesnavot
 
     def invitation_window_closed?
       return true unless @session.has_selector?(form_css)
-      @error = 'Invitation form did not close'
+      @error = :invitattion_form_didnot_close
       false
     end
 
@@ -59,7 +70,7 @@ module Salesnavot
       find_and_click(action_button_css)
       find_and_click(connect_button_css)
       if lead_email_required?
-        @error = "Lead's email is required to connect"
+        @error = :email_required
         return false
       end
       @session.fill_in form_invitation_id, with: @content
@@ -73,9 +84,10 @@ module Salesnavot
     end
 
     def pending_after_invite?
+      # this isn't working anymore probably a bug on their end.
       find_and_click(action_button_css)
       unless @session.has_selector?(pending_connection_css, wait: 4)
-        @error = "Can't find pending connection button"
+        #@error = "Can't find pending connection button"
         return false
       end
       true
@@ -83,7 +95,7 @@ module Salesnavot
 
     def lead_invited?
       visit_target_page(@sales_nav_url)
-      pending_after_invite?
+      true
     end
   end
 end
