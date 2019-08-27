8992a0607c38e5b6af29466b0fb639545b01f605
Sébastien CEBULA <sebastien.cebula@gmail.com>
2019-08-27 16:21:15 +0200

    Some fixes in sales nav threads unit tests

--- spec/unit/sales_nav_threads_spec.rb
+++ spec/unit/sales_nav_threads_spec.rb
@@ -11,8 +11,6 @@ RSpec.describe ScrapIn::SalesNavigator::Threads do
     subject.new(session)
   end
 
-  def node_array(size); end
-
   let(:session) { instance_double('Capybara::Session') }
   let(:threads_list) { instance_double('Capybara::Node::Element') }
   let(:threads_list_elements) { instance_double('Capybara::Node::Element') }
@@ -22,6 +20,22 @@ RSpec.describe ScrapIn::SalesNavigator::Threads do
   let(:threads_list_array) { [] }
   let(:thread_name_array) { [] }
   let(:one_message_array) { [] }
+  let(:thread_names) do
+    [
+      'Thread name 1',
+      'Thread name 2',
+      'Thread name 3'
+    ]
+  end
+
+  let(:names_array) do
+    [
+      'Name 1',
+      'Name 2',
+      'Name 3'
+    ]
+  end
+
 
   include CssSelectors::SalesNavigator::Threads
   before do
@@ -29,41 +43,42 @@ RSpec.describe ScrapIn::SalesNavigator::Threads do
     disable_sleep_for_class(ScrapIn::SalesNavigator::Threads)
 
     has_selector(session, threads_access_button_css, wait: 5)
-    allow(session).to receive(:has_selector?).and_return(true)
     create_node_array(thread_access_button_array)
     create_node_array(message_array, 3)
     create_node_array(loaded_threads_array, 3)
     create_node_array(thread_name_array, 3)
     create_node_array(one_message_array)
 
-    allow(session).to receive(:has_selector?).with(message_css, wait: 5).and_return(true)
+    has_selector(session, message_css, wait: 5)
+
     allow(session).to receive(:all).with(threads_access_button_css, wait: 5).and_return(thread_access_button_array)
 
     allow(thread_access_button_array[0]).to receive(:click)
 
-    allow(session).to receive(:has_selector?).with(message_css, wait: 5).and_return(true)
+    has_selector(session, message_css, wait: 5)
     allow(session).to receive(:all).with(message_css, wait: 5).and_return(message_array)
 
-    allow(session).to receive(:has_selector?).with(threads_list_css).and_return(true)
-    allow(session).to receive(:find).with(threads_list_css).and_return(threads_list)
+    has_selector(session, threads_list_css)
 
-    allow(threads_list).to receive(:has_selector?).with(loaded_threads_css, wait: 5).and_return(true)
+    find(session, threads_list_css, threads_list)
+
+    has_selector(threads_list, loaded_threads_css, wait: 5)
     allow(threads_list).to receive(:all).with(loaded_threads_css, wait: 5).and_return(loaded_threads_array)
 
-    allow(threads_list).to receive(:has_selector?).with(threads_list_elements_css, wait: 5).and_return(true)
+    has_selector(threads_list, threads_list_elements_css, wait: 5)
     allow(threads_list).to receive(:all).with(threads_list_elements_css, wait: 5).and_return(threads_list_array)
 
     create_node_array(threads_list_array, 3)
     # thread_name_array
     count = 0
     threads_list_array.each do |threads_list_elements|
-      allow(threads_list_elements).to receive(:has_selector?).with(thread_name_css, wait: 5).and_return(true)
+      has_selector(threads_list_elements, thread_name_css, wait: 5)
       allow(threads_list_elements).to receive(:find).with(thread_name_css, wait: 5).and_return(thread_name_array[count])
-      allow(thread_name_array[count]).to receive(:text)
+      allow(thread_name_array[count]).to receive(:text).and_return(names_array[count])
       allow(thread_name_array[count]).to receive(:click)
-      allow(session).to receive(:current_url).and_return('Thread url')
       count += 1
     end
+    allow(session).to receive(:current_url).and_return(*thread_names)
   end
 
   describe '.initialize' do
@@ -71,9 +86,14 @@ RSpec.describe ScrapIn::SalesNavigator::Threads do
   end
 
   describe '.execute' do
-    context 'everything is ok in order to scrap threads links' do
+    context 'when everything is ok in order to scrap threads links' do
       it 'scraps successfully threads and names' do
-        result = sales_nav_threads_instance.execute { |_name, _thread_link| }
+        count = 0
+        result = sales_nav_threads_instance.execute do |name, thread_link| 
+          expect(thread_link).to eq(thread_names[count])
+          expect(name).to eq(names_array[count])
+          count += 1
+        end
         expect(result).to be(true)
       end
     end
@@ -87,7 +107,12 @@ RSpec.describe ScrapIn::SalesNavigator::Threads do
 
     context 'num_times eq 1' do
       it 'scraps one thread' do
-        result = sales_nav_threads_instance.execute(1) { |_name, _thread_link| }
+        count = 0
+        result = sales_nav_threads_instance.execute(1) do |name, thread_link| 
+          expect(thread_link).to eq(thread_names[count])
+          expect(name).to eq(names_array[count])
+          count += 1
+        end
         expect(result).to be(true)
       end
     end
@@ -104,14 +129,14 @@ RSpec.describe ScrapIn::SalesNavigator::Threads do
 
     context 'the selector for threads list was not found' do
       before { has_not_selector(session, threads_list_css) }
-      it {
+      it do
         expect { sales_nav_threads_instance.execute { |_name, _thread_link| } }
           .to raise_error(ScrapIn::CssNotFound)
-      }
-      it {
+      end
+      it do
         expect { sales_nav_threads_instance.execute { |_name, _thread_link| } }
           .to raise_error(/#{threads_list_css}/)
-      }
+      end
     end
 
     context 'the selector for threads list elements was not found' do
