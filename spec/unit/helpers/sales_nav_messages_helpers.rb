291918fc5b689f07b8f4f1b1a706fa71aa6c8c12
Sébastien CEBULA <sebastien.cebula@gmail.com>
2019-08-27 17:04:55 +0200

    Modified a bit of salesNavMessages helpers

--- spec/unit/helpers/sales_nav_messages_helpers.rb
+++ spec/unit/helpers/sales_nav_messages_helpers.rb
@@ -1,10 +1,17 @@
 # Helpers for SalesNavMessages unit tests
 module SalesNavMessagesHelpers
+  def message_content(count)
+    "Message content #{count}"
+  end
+
   def create_conversation(array)
     array.each_with_index do |element, count|
       has_selector(element, content_css, wait: 5)
       has_selector(element, sender_css, wait: 2, visible: false)
-      content = { 'innerHTML' => "Message content #{count}" }
+
+      content = { 'innerHTML' => message_content(count) }
+      content = { 'innerHTML' => "beginnning of the conversation" } if count.zero?
+
       # Alternate senders to see incoming and outgoing directions
       sender = count.even? ? { 'innerHTML' => '   You   ' } : { 'innerHTML' => '   Sender   ' }
       allow(element).to receive(:find).with(content_css, wait: 5).and_return(content)
