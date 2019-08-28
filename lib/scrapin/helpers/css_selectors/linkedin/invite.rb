5bb757baed085acb2d4974a9e6bd7b7507f89bbe
Valentin Piatko <valentin@gembani.com>
2019-08-28 17:37:54 +0200

    Move unit tests Rename all css_selectors with '..._css'

--- lib/scrapin/helpers/css_selectors/linkedin/invite.rb
+++ lib/scrapin/helpers/css_selectors/linkedin/invite.rb
@@ -1,27 +1,27 @@
 module CssSelectors
 	module LinkedIn
 		module Invite
-			def degree
+			def degree_css
 				'.dist-value'
 			end
 
-			def connect_button
+			def connect_button_css
 				'.artdeco-button__text'
 			end
 
-			def more_button
+			def more_button_css
 				'.artdeco-button__text'
 			end
 
-			def connect_in_more_button
+			def connect_in_more_button_css
 				'.display-flex t-normal pv-s-profile-actions__label'
 			end
 
-			def send_now_button
+			def send_now_button_css
 				'.artdeco-button artdeco-button--3 ml1'
 			end
 
-			def confirm_invite
+			def confirm_invite_css
 				'.t-14 t-black--light t-normal ph2'
 			end
 		end
--- lib/scrapin/linkedin/invite.rb
+++ lib/scrapin/linkedin/invite.rb
@@ -9,18 +9,18 @@ module ScrapIn
 			
 			def execute(lead_url)
 				visit_lead_url(lead_url)
-				deg = check_and_find_all(@session, degree).first.text
+				deg = check_and_find_all(@session, degree_css).first.text
 				puts "#{deg} degree"
 				if (deg == '2nd')
-					check_and_find_all(@session, connect_button).first.click
+					check_and_find_all(@session, connect_button_css).first.click
 					puts 'Click on Connect...'
 				elsif (deg == '3rd')
-					check_and_find(@session, more_button).click
-					check_and_find(@session, connect_in_more_button).click
+					check_and_find(@session, more_button_css).click
+					check_and_find(@session, connect_in_more_button_css).click
 				end
 				byebug
 				check_and_find_all(send_now_button).first.click
-				return false unless check_and_find(@session, confirm_invite)
+				return false unless check_and_find(@session, confirm_invite_css)
 				true
 			end
 
