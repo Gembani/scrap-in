c43dd58af9916b92ead0b013f833a8a133861dee
Valentin Piatko <valentin@gembani.com>
2019-09-03 12:41:22 +0200

    created click_button

--- lib/scrapin/linkedin/invite.rb
+++ lib/scrapin/linkedin/invite.rb
@@ -11,10 +11,10 @@ module ScrapIn
 				visit_lead_url(lead_url)
 				buttons = check_and_find_all(@session, buttons_css)
 				puts 'Search for Connect button'
-				connect_worked = click_connect(buttons)
+				connect_worked = click_button(buttons, 'Connect')
 				unless connect_worked
 					puts 'Connect not found. Search for More… button'
-					click_more(buttons)
+					click_button(buttons, 'More…')
 					puts 'Search for Connect button'
 					check_and_find_all(@session, connect_in_more_button_css, visible: false)[3].click
 				end
@@ -22,73 +22,23 @@ module ScrapIn
 				new_buttons = check_and_find_all(@session, buttons_css)
 				byebug
 				if note.empty?
-					click_send_now(new_buttons)
+					click_button(new_buttons, 'Send now')
 				else
-					click_add_note(new_buttons)
+					click_button(new_buttons, 'Add a note')
 				end
 				true
 			end
 			
-			def click_connect(buttons)
-				buttons.each do |button|
-					if button.text == 'Connect'
+			def click_button(buttons_array, button_name)
+				buttons_array.each do |button|
+					if button.text == button_name
 						button.click
-						puts 'Clicked on Connect'
+						puts "Clicked on #{button_name}"
 						return true
 					end
 				end
 				return false
 			end
-	
-			def click_more(buttons)
-				buttons.each do |button|
-					if button.text == 'More…'
-						button.click
-						puts 'Clicked on More…'
-						return true
-					end
-				end
-				return false
-			end
-
-			def click_send_now(buttons)
-				buttons.each do |button|
-					if button.text == 'Send now'
-						button.click
-						puts 'Clicked on Send now'
-						return true
-					end
-				end
-				return false
-			end
-
-			def click_add_note(buttons)
-				buttons.each do |button|
-					if button.text == 'Add a note'
-						button.click
-						puts 'Clicked on Add a note'
-						return true
-					end
-				end
-				return false
-			end
-			# def execute(lead_url)
-			# 	visit_lead_url(lead_url)
-			# 	degree = check_and_find_all(@session, degree_css).first.text
-			# 	puts "#{degree} degree"
-			# 	# byebug
-			# 	if (degree == '2nd')
-			# 		check_and_find_all(@session, connect_button_css).first.click
-			# 		puts 'Click on Connect...'
-			# 	elsif (degree == '3rd')
-			# 		check_and_find(@session, more_button_css).click
-			# 		check_and_find(@session, connect_in_more_button_css).click
-			# 	end
-			# 	@session.all('.artdeco-button')[4].click
-			# 	# check_and_find_all(send_now_button_css)[4].click
-			# 	return false unless check_and_find(@session, confirm_invite_css)
-			# 	true
-			# end
 
 			def visit_lead_url(lead_url)
 				@session.visit(lead_url)
