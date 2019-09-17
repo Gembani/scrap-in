3a08321fb70321271b6bfa37c154d49cbe90fb2e
Valentin Piatko <valentin@gembani.com>
2019-09-17 16:53:18 +0200

    w

--- spec/unit/linkedin/threads_spec.rb
+++ spec/unit/linkedin/threads_spec.rb
@@ -16,35 +16,65 @@ RSpec.describe ScrapIn::LinkedIn::Threads do
 	let(:item_href_hash) { { href: 'https://www.linkedin.com/messaging/threads/johnsmith' } }
 	let(:threads_block_array) { [] }
 	let(:item_array) { [] }
+	let(:item) {}
 
 	include CssSelectors::LinkedIn::Threads
 
 	before do
+		disable_puts_for_class(ScrapIn::LinkedIn::Threads)
 		disable_script
 		create_node_array(threads_block_array, 3)
-		create_node_array(item_array, 3)
+		create_node_array(item_array, 12)
 
 		allow(session).to receive(:visit).with(messages_link)
 
 		has_selector(session, threads_block_css)
 		allow(session).to receive(:all).with(threads_block_css).and_return(threads_block_array)
 
-		count = 0
-		12.times.each do 
+		count = -1
+		12.times do 
 			has_selector(session, threads_block_count_css(count))
 			allow(session).to receive(:all).with(threads_block_count_css(count)).and_return(item_array)
 			item = item_array.first
+			
 			has_selector(item, one_thread_css)
 			allow(item).to receive(:find).with(one_thread_css).and_return(thread_name)
 			allow(thread_name).to receive(:text).and_return('John Smith')
 			name = thread_name
+			
 			has_selector(item, href_css)
 			allow(item).to receive(:find).with(href_css).and_return(item_href_hash)
 			link = item_href_hash[:href]
+			
 			allow(item).to receive(:native)
+			# puts(count)
 			count += 1
 		end
 	end
+
+	# context 'the selector for threads block count was not found' do
+	# 	before do
+	# 		count = 0
+	# 		12.times do
+	# 			has_not_selector(session, threads_block_count_css(count))
+	# 			count += 1
+	# 		end
+	# 	end
+	# 	it do
+	# 		12.times do
+	# 			expect { linkedin_threads_instance.execute { |_name, _thread_link| } }
+	# 				.to raise_error(ScrapIn::CssNotFound)
+	# 			count += 1
+	# 		end
+	# 	end
+	# 	it do
+	# 		12.times do
+	# 			expect { linkedin_threads_instance.execute { |_name, _thread_link| } }
+	# 				.to raise_error(/#{threads_block_count_css}/)
+	# 			count += 1
+	# 		end
+	# 	end
+	# end
 
 	context 'the selector for threads block was not found' do
 		before { has_not_selector(session, threads_block_css) }
@@ -59,7 +89,7 @@ RSpec.describe ScrapIn::LinkedIn::Threads do
 	end
 
 	context 'the selector for one thread was not found' do
-		before { has_not_selector(session, one_thread_css) }
+		before { has_not_selector(item, one_thread_css) }
 		it do
 			expect { linkedin_threads_instance.execute { |_name, _thread_link| } }
 				.to raise_error(ScrapIn::CssNotFound)
@@ -71,7 +101,7 @@ RSpec.describe ScrapIn::LinkedIn::Threads do
 	end
 
 	context 'the selector for href was not found' do
-		before { has_not_selector(session, href_css) }
+		before { has_not_selector(item, href_css) }
 		it do
 			expect { linkedin_threads_instance.execute { |_name, _thread_link| } }
 				.to raise_error(ScrapIn::CssNotFound)
