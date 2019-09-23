249668f1073d77b69551da7a5731823259776ae0
Valentin Piatko <valentin@gembani.com>
2019-09-23 15:12:47 +0200

    threads

--- lib/scrapin/sales_navigator/threads.rb
+++ lib/scrapin/sales_navigator/threads.rb
@@ -1,5 +1,6 @@
 module ScrapIn
   module SalesNavigator
+    # Scrap threads
     class Threads
       include ScrapIn::Tools
       include CssSelectors::SalesNavigator::Threads
@@ -9,6 +10,7 @@ module ScrapIn
 
       def execute(num_times = 500)
         return true if num_times.zero?
+
         visit_messages_link
         count = 0
 
@@ -19,6 +21,8 @@ module ScrapIn
             break
           else
             conversation = find_conversation(count)
+            return false if conversation == false
+
             name = conversation.text
             conversation.click
             thread_link = @session.current_url
@@ -31,7 +35,7 @@ module ScrapIn
       end
 
       def visit_messages_link
-        @session.all(threads_access_button_css, wait: 5)[0].click
+        check_and_find_first(@session, threads_access_button_css, wait: 5).click
         wait_messages_page_to_load
         puts 'Messages have been visited.'
       end
@@ -49,7 +53,8 @@ module ScrapIn
       def find_conversation(count)
         threads_list = check_and_find(@session, threads_list_css)
         threads_list_elements = threads_list.all(threads_list_elements_css, wait: 5)[count]
-        # byebug
+        return false if threads_list_elements.nil?
+
         check_and_find(threads_list_elements, thread_name_css, wait: 5)
       end
 
