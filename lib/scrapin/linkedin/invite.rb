3827ab996401719aab436a4f67891f3abb50f59a
vapiatko <33578852+vapiatko@users.noreply.github.com>
2019-09-24 11:48:21 +0200

    Delete check_and_find_all (#16)
    * deleted check_and_find_all from tools
    deleted check_and_find_all from all classes using it
    
    * send_message
    
    * messages
    
    Co-authored-by: qsebasti <squemere77@gmail.com>
    
    * invite
    
    Co-authored-by: qsebasti <squemere77@gmail.com>
    
    * threads
    
    * remove comments in unit tests

--- lib/scrapin/linkedin/invite.rb
+++ lib/scrapin/linkedin/invite.rb
@@ -6,49 +6,65 @@ module ScrapIn
 			def initialize(session, lead_url)
         @session = session
 			end
-			
+
 			def execute(lead_url, note = '')
 				visit_lead_url(lead_url)
-				buttons = check_and_find_all(@session, buttons_css)
+				return false unless search_for_connect_button
+				search_for_add_a_note_button(note)
+				sending_invitation_message
+				confirmation_invite_is_sent
+				true
+			end
+			
+			def visit_lead_url(lead_url)
+				@session.visit(lead_url)
+				puts 'Lead profile has been visited'
+			end
+
+			def search_for_connect_button
+				buttons = @session.all(buttons_css)
 				puts 'Search for Connect button'
 				connect_worked = click_button(buttons, 'Connect')
 				unless connect_worked
 					puts 'Connect not found. Search for More… button'
-					click_button(buttons, 'More…')
+					return false unless click_button(buttons, 'More…')
 					puts 'Search for Connect button'
-					check_and_find_all(@session, connect_in_more_button_css, visible: false)[3].click
+					buttons = @session.all(connect_in_more_button_css, visible: false)
+					connect_worked = click_button(buttons, 'Connect')
 				end
-				puts 'Search for Send now button'
-				buttons_popup = check_and_find_all(@session, buttons_css)
-				if note.empty?
-					click_button(buttons_popup, 'Send now')
-				else
-					click_button(buttons_popup, 'Add a note')
-					input_note_area = check_and_find(@session, note_area_css)
-					input_note_area.send_keys(note)
-					puts 'Sending invitation message'
-					new_buttons_popup = check_and_find_all(@session, buttons_css)
-					click_button(new_buttons_popup, 'Send invitation')
-					puts 'Message sent'
-				end
-				return true if @session.find('span', text: confirmation_text)
-				false
+				connect_worked
+			end
+
+			def search_for_add_a_note_button(note)
+				puts 'Search for add_a_note button'
+				buttons_popup = @session.all(buttons_css)
+				return false unless click_button(buttons_popup, 'Add a note')
+				input_note_area = check_and_find(@session, note_area_css)
+				input_note_area.send_keys(note)
 			end
 			
+			def sending_invitation_message
+				puts 'Sending invitation message'
+				new_buttons_popup = @session.all(buttons_css)
+				click_button(new_buttons_popup, 'Send invitation')
+			end
+
+			def confirmation_invite_is_sent
+				return true if check_and_find(@session, 'span', text: confirmation_text)
+				byebug
+				false
+			end
+
+			private
 			def click_button(buttons_array, button_name)
 				buttons_array.each do |button|
-					if button.text == button_name
+					if button.text.include?(button_name)
 						button.click
 						puts "Clicked on #{button_name}"
 						return true
 					end
 				end
 				return false
-			end
-
-			def visit_lead_url(lead_url)
-				@session.visit(lead_url)
-				puts 'Lead profile has been visited'
 			end
 		end
 	end
