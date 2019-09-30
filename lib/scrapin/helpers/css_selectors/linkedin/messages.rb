e134b41c317f2a54e9027a9d9d200b711b131b0b
Sébastien QUEMERE <qseb@gembani.com>
2019-09-30 12:15:14 +0200

    unit test almost done

--- lib/scrapin/helpers/css_selectors/linkedin/messages.rb
+++ lib/scrapin/helpers/css_selectors/linkedin/messages.rb
@@ -2,36 +2,20 @@ module CssSelectors
   module LinkedIn
     # All css selectors used in ScrapIn::LinkedIn::Messages Class
     module Messages
-      def message_number_css(count)
-        ".msg-s-message-list li:nth-child(#{count + 2}) .msg-s-event-listitem"
-      end
-
-      def unknown_css
-        '.button-primary-small'
-      end
-
-      def messages_list_css
-        '.msg-s-event-listitem__message-bubble'
-      end
-
       def message_content_css
         '.msg-s-event-listitem__body'
-      end
-
-      def message_direction_css
-        '.msg-s-event-listitem--other'
       end
 
       def messages_thread_css
         '.msg-s-message-list-container'
       end
 
-      def same_messages_list_css
-        '.msg-s-event-listitem'
+      def sender_css
+        'msg-s-event-listitem--other'
       end
 
-      def lead_name_css
-        '.msg-entity-lockup__entity-title'
+      def all_messages_css
+        '.msg-s-message-list li .msg-s-event-listitem'
       end
     end
   end
--- lib/scrapin/linkedin/messages.rb
+++ lib/scrapin/linkedin/messages.rb
@@ -10,57 +10,24 @@ module ScrapIn
       end
       
       def execute(number_of_messages = 20)
-        return false unless number_of_messages.positive?
+        return false unless number_of_messages.positive? # ArgumentError.new
 
         return false unless visit_thread_link
+
         loaded_messages = load(number_of_messages)
+        return false if loaded_messages.zero?
+
         count = loaded_messages - 1
         loaded_messages.times.each do
-          message = @session.all('.msg-s-message-list li .msg-s-event-listitem')
-          content = check_and_find(message[count], '.msg-s-event-listitem__body').text
-          direction = message.include?('msg-s-event-listitem--other') ? :incoming : :outgoing
+          message = @session.all(all_messages_css)
+          content = check_and_find(message[count], message_content_css).text
+          direction = message[count][:class].include?(sender_css) ? :incoming : :outgoing
           count -= 1
           yield content, direction
         end
+        true
       end
-      # def css(count)
-      #   ".msg-s-message-list li:nth-child(#{count + 2}) .msg-s-event-listitem"
-      # end
-      
-      # def can_send_greeting_message
-      #   number_of_messages = 0
-      #   execute(2) do |_message, _direction|
-      #     number_of_messages += 1
-      #   end
-      #   number_of_messages == 1
-      # end
-
-      # def execute(number_of_messages = 20)
-      #   visit_thread_link
-      #   count = 0
-        
-      #   number_of_messages.times.each do
-      #     item = @session.all(message_number_css(0)).first
-      #     scroll_to(item)
-      #     sleep(1)
-      #   end
-        
-      #   number_of_messages.times.each do
-      #     item = @session.all(message_number_css(count)).last
-      #     count += 1
-      #     if item.nil?
-      #       count = 0
-      #       break
-      #     else
-      #       next unless item.all(messages_list_css).count == 1
-      #       message = item.find(message_content_css).text
-      #       direction = item[:class].include?(message_direction_css) ? :incoming : :outgoing
-      #     end
-      #     yield message, direction
-      #   end
-      #   sleep(0.5)
-      # end
-      
+    
       def visit_thread_link
         @session.visit(@thread_link)
         return false unless wait_messages_to_appear
@@ -78,8 +45,8 @@ module ScrapIn
 
       def load(number_of_messages)
         loaded_messages = count_loaded_messages
-        while loaded_messages < number_of_messages && loaded_messages > 0
-          first_message = check_and_find_first(@session, '.msg-s-message-list li .msg-s-event-listitem')
+        while loaded_messages < number_of_messages && loaded_messages.positive?
+          first_message = check_and_find_first(@session, all_messages_css)
           check_until(500) do
             scroll_to(first_message)
           end
@@ -91,47 +58,8 @@ module ScrapIn
       end
 
       def count_loaded_messages
-        check_and_find_all(@session, '.msg-s-message-list li .msg-s-event-listitem').count
+        @session.all(all_messages_css).count
       end
-      
-      # def get_messages_blocks
-      #   messages_blocks = @session.all(same_messages_list_css).last(@number_of_messages)
-      #   while messages_blocks.count < @number_of_messages
-      #     puts 'Searching for more messages ...'
-      #     # only 20 messages are loaded at the beginning
-      #     scroll_to(messages_blocks.first)
-      #     sleep(1)
-      #     tmp = messages_blocks.count
-      #     messages_blocks = @session.all(same_messages_list_css).last(@number_of_messages)
-      #     break if tmp == messages_blocks.count
-      #   end
-      #   messages_blocks
-      # end
-      
-      # def parse(messages_blocks)
-      #   messages_blocks.each do |block|
-      #     next unless block.all(messages_list_css).count == 1
-      #     message = block.find(message_content_css).text
-      #     hash = if block[:class].include?(message_direction_css)
-      #       { direction: :incoming, message: message }
-      #     else
-      #       { direction: :outgoing, message: message }
-      #     end
-          
-      #     @all_messages.push(hash)
-      #   end
-      # end
-      
-      # def display_messages
-      #   name = @session.all(lead_name_css).first.text
-      #   @all_messages.each do |message|
-      #     if message[:direction] == :incoming
-      #       puts name + ' -> ' + message[:message]
-      #     else
-      #       puts 'You -> ' + message[:message]
-      #     end
-      #   end
-      # end
     end
   end
 end
