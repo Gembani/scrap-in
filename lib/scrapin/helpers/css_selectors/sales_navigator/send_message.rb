b6162bf4b91aed0e941ecbd5c6cd425e9888a090
qsebasti <38917535+qsebasti@users.noreply.github.com>
2019-10-05 15:15:11 +0200

    Salesnav send message 168779408 (#23)
    * first commit, need raise error in send_message
    
    * wip spec
    
    * [Finishes 168779408]
    
    * more precisions in context

---
+++ lib/scrapin/helpers/css_selectors/sales_navigator/send_message.rb
@@ -0,0 +1,17 @@
+module CssSelectors
+  module SalesNavigator
+    module SendMessage
+      def message_field_css
+        '.compose-form__message-field'
+      end
+
+      def send_button_css
+        '.artdeco-button--primary'
+      end
+
+      def messages_css
+        '.ml8'
+      end
+    end
+  end
+end
---
+++ lib/scrapin/sales_navigator/send_message.rb
@@ -0,0 +1,58 @@
+module ScrapIn
+  module SalesNavigator
+    class SendMessage
+      include Tools
+      include CssSelectors::SalesNavigator::SendMessage
+      def initialize(session, thread, message)
+        @session = session
+        @thread = thread
+        @message = message
+      end
+
+      def execute
+        return false unless visit_thread
+
+        write_message
+        send_message
+        message_sent?
+      end
+
+      def visit_thread
+        @session.visit(@thread)
+        wait_messages_to_appear
+      end
+
+      def wait_messages_to_appear
+        puts 'waiting messages to appear'
+        messages_appear = check_until(500) do
+          @session.all(messages_css).count.positive?
+        end
+        messages_appear
+      end
+
+      def write_message
+        puts 'Writing message...'
+        message_field = check_and_find(@session, message_field_css)
+        message_field.send_keys(@message)
+        puts 'Message has been written.'
+      end
+
+      def send_message
+        puts 'Sending message...'
+        find_and_click(@session, send_button_css)
+        puts 'Message has been sent.'
+        true
+      end
+
+      def message_sent?
+        puts 'Checking the message has been sent...'
+        return false if @session.all(messages_css)[-1].nil?
+
+        return false if @session.all(messages_css)[-1].text != @message
+
+        puts 'Confirmed'
+        true
+      end
+    end
+  end
+end
