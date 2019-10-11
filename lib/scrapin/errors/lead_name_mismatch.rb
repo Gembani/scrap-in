718a2709942f65ebbe770a0731df088d88f51e47
Sébastien CEBULA <sebastien.cebula@gmail.com>
2019-10-11 12:23:31 +0200

    Added new custom error

---
+++ lib/scrapin/errors/lead_name_mismatch.rb
@@ -0,0 +1,9 @@
+module ScrapIn
+  class LeadNameMismatch < StandardError
+
+    def initialize(lead_name_in_thread, lead_name)
+      @message = "Lead name mismatch. Lead in thread = #{lead_name_in_thread} / Lead name given = #{lead_name}"
+      super(@message)
+    end
+  end
+end
