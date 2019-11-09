61d0c29522dd38fb7a72e0d3f2a3260e1fa2b180
Nicholas Stock <nick@gembani.com>
2019-11-09 15:56:10 +0100

    fixed this

--- lib/scrapin/session.rb
+++ lib/scrapin/session.rb
@@ -4,10 +4,14 @@ module ScrapIn
     include LinkedInMethods
     include SalesNavigatorMethods
 
-    def initialize(username, password, linkedin = false)
+    def initialize(username, password, linkedin = false, capybara = false)
       Capybara.default_max_wait_time = 10 # Seconds
       # Capybara.default_max_wait_time = 60 # Seconds
-      @capybara = Capybara::Session.new(ENV.fetch('driver').to_sym)
+      if (capybara)
+        @capybara = capybara
+      else
+        @capybara = Capybara::Session.new(ENV.fetch('driver').to_sym)
+      end
       auth = ScrapIn::Auth.new(@capybara)
       auth.login!(username, password, linkedin)
     end
